2bd6876b5c4bea74c051b953c0b8d2880b0ce39c
david combita <combita.1105@gmail.com>
2023-03-13 12:58:49 -0500

    add: first commit, create micro-service

---
+++ microservice-classes-registration/Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace microservice_classes_registration.Controllers;
+
+[ApiController]
+[Route("students")]
+public class StudentsController : ControllerBase{
+
+    private readonly ILogger<StudentsController> _logger;
+
+    public StudentsController(ILogger<StudentsController> logger){
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "testConnection")]
+    public System.String testConnection(){
+        return $"Test Connection {DateTime.Now.ToString("dd/MM/yy hh:mm:ss")}.";
+    }
+
+}
